800c722fcfe46fe624f6ae9a06d0fa9ba98eab62
Denys Davydov <davydov654@gmail.com>
2023-03-31 16:18:47 +0300

    Tomcat plugin added and fixed http statutes

--- src/main/java/com/epam/esm/config/WebConfig.java
+++ src/main/java/com/epam/esm/config/WebConfig.java
@@ -6,7 +6,6 @@ import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
